191007a60450bdc901a3fcb177825b7fce3d356b
RonnaldHaro7 <rsa.haro@yavirac.edu.ec>
2023-01-28 14:00:04 -0500

    feature: first commit

---
+++ src/main/java/uic/api_anteproyecto/ApiAnteproyectoApplication.java
@@ -0,0 +1,13 @@
+package uic.api_anteproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnteproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnteproyectoApplication.class, args);
+	}
+
+}
