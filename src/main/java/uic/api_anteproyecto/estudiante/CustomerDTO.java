2b31536922686891a8bbaf98dddaf24861db72ba
RonnaldHaro7 <rsa.haro@yavirac.edu.ec>
2023-01-29 17:47:25 -0500

    feature: pdf v1

---
+++ src/main/java/uic/api_anteproyecto/estudiante/CustomerDTO.java
@@ -0,0 +1,10 @@
+package uic.api_anteproyecto.estudiante;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String nro_identificacion;
+}
