770ce78b3d53b4d372e9b67520e0ccad2986b67c
RonnaldHaro7 <rsa.haro@yavirac.edu.ec>
2023-01-29 23:51:55 -0500

    feature: pdf v2.1

--- src/main/java/uic/api_anteproyecto/generarsolicitudpdf/Solicitud.java
+++ src/main/java/uic/api_anteproyecto/generarsolicitudpdf/Solicitud.java
@@ -1,26 +1,18 @@
 package uic.api_anteproyecto.generarsolicitudpdf;
 
-import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-
 import lombok.Data;
 
 @Data
 @Entity
-class SolicitudPDFLinea {
+class Solicitud {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String tema;
 
-    @ManyToOne
-    private SolicitudPDF solicitudpdf;
-
-    private String nombre_profesor;
-    private String periodo_lectivo;
-    private Date fecha;
 }
